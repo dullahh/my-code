e5a80f78c9bdf58e8810ce3e7608dd0e792842a4
dullahh <151881445+dullahh@users.noreply.github.com>
2023-12-10 11:38:41 +0000

    Add files via upload

---
+++ quarter colours.py
@@ -0,0 +1,184 @@
+from turtle import *
+speed(0)
+penup()
+setheading(90)
+colormode(255)
+width(4)
+
+r = 255
+g = 0
+b = 0
+
+tracer(0,0)
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r -= 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    b += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    b -= 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    g += 1
+    b += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r -= 1
+    g -= 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r += 1
+    g += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    b -= 1
+    g -= 1
+
+width(1)
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r -= 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    b += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    b -= 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    g += 1
+    b += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+    r -= 1
+    g -= 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+
+    r += 1
+    g += 1
+
+for i in range(255):
+
+    pencolor(r,g,b)
+    pendown()
+    fd(900)
+    penup()
+    goto(0,0)
+    rt(45/255)
+
+    b -= 1
+    g -= 1
